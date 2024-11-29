236feac4dd89ea693e6ca29553317df82cc588f5
yoni <johnysalvadorcampos@gmail.com>
2024-11-28 22:30:55 -0600

    frontend

---
+++ src/app/components/contenido/contenido.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'; 
+import { ContenidoService } from '../../services/usuario.service'; 
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contenido',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contenido.component.html',
+  styleUrls: ['./contenido.component.css']
+})
+export class ContenidoComponent implements OnInit {
+  arrayClientes: any[] = []; 
+  nuevoClienteForm: FormGroup; 
+  clienteEnEdicion: any = null; 
+
+  constructor(private contenidoService: ContenidoService, private fb: FormBuilder) {
+    this.nuevoClienteForm = this.fb.group({
+      nombre: [''],
+      direccion: [''],
+      tipo: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchClientes();
+  }
+
+  fetchClientes(): void {
+    this.contenidoService.fetchClientes().subscribe(
+      (result: any) => {
+        this.arrayClientes = result;
+      },
+      (error) => {
+        console.error('Error fetching clientes:', error);
+      }
+    );
+  }
+
+  crearCliente(): void {
+    const cliente = this.nuevoClienteForm.value; 
+    this.contenidoService.postCliente(cliente).subscribe(
+      (result) => {
+        this.arrayClientes.push(result); 
+        this.nuevoClienteForm.reset(); 
+      },
+      (error) => {
+        console.error('Error creando película:', error);
+      }
+    );
+  }
+
+  editarCliente(cliente: any): void {
+    this.clienteEnEdicion = cliente;
+    this.nuevoClienteForm.patchValue(cliente);
+  }
+
+  actualizarCliente(): void {
+    const clienteActualizada = this.nuevoClienteForm.value;
+    this.contenidoService.updateCliente(this.clienteEnEdicion.id_cliente, clienteActualizada).subscribe(
+      (result) => {
+        const index = this.arrayClientes.findIndex(p => p.id_cliente === this.clienteEnEdicion.id_cliente);
+        if (index !== -1) {
+          this.arrayClientes[index] = result;
+        }
+        this.clienteEnEdicion = null;
+        this.nuevoClienteForm.reset();
+      },
+      (error) => {
+        console.error('Error actualizando película:', error);
+      }
+    );
+  }
+
+  eliminarCliente(id_cliente: string): void {
+    this.contenidoService.deleteCliente(id_cliente).subscribe(
+      () => {
+        this.arrayClientes = this.arrayClientes.filter(p => p.id_cliente !== id_cliente);
+      },
+      (error) => {
+        console.error('Error eliminando película:', error);
+      }
+    );
+  }
+}
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContenidoService {
+  private apiUrl = 'https://backendjosue.onrender.com/api/lugares';
+
+  constructor(private http: HttpClient) { }
+  // Obtener todos los clientes
+  fetchClientes(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Obtener una película por ID
+  fetchClienteById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  // Crear una nueva película
+  postCliente(cliente: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, cliente);
+  }
+
+  // Actualizar una película
+  updateCliente(id_cliente: string, cliente: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id_cliente}`, cliente);
+  }
+
+  // Eliminar una película
+  deleteCliente(id_cliente: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id_cliente}`);
+  }
+}
